9a0683087f2a4215e8f92fa1c5fb85cb0a780e84
unknown <tanishkabadhe.cps@gmail.com>
2020-08-01 12:41:52 +0530

    Updates

---
+++ precache-manifest.7af4665e159e35cd2cc2498188410b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2586dac69aa7170f736f90b5546994",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bc649801a72769a87d5a",
+    "url": "/react-app/static/css/main.0e04fa91.chunk.css"
+  },
+  {
+    "revision": "d2a70f48db035e5b318f",
+    "url": "/react-app/static/js/2.297387de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.297387de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc649801a72769a87d5a",
+    "url": "/react-app/static/js/main.ee0bdafd.chunk.js"
+  },
+  {
+    "revision": "32d10845f7090ff5999c",
+    "url": "/react-app/static/js/runtime-main.1c3dba08.js"
+  }
+]);
